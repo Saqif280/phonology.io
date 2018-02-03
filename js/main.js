cc6c52eb0207abbe52883ced54a00fd7b03760db
Saqif Badruddin <saqif280@gmail.com>
2018-02-03 18:58:03 -0500

    fixed number and div bug

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+// slide to id
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+	
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate({
+		scrollTop: position
+	} /* speed */ );
+});
+
 // word sets
 var numToAdj = [];
 var adjToNum = {};
@@ -5,19 +16,29 @@ var numToNoun = [];
 var nounToNum = {};
 
 // read in txt files
-d3.csv("../data/adjectives.txt", function(data) {
+d3.csv("../data/medium-10k.txt", function(data) {
 	// populate sets
 	data.forEach(function(d,i) {
   	numToAdj.push(d.word);
   	adjToNum[d.word] = i;
 	});
 });
+d3.csv("../data/all-10k.txt", function(data) {
+	// populate sets
+	data.forEach(function(d,i) {
+  	numToNoun.push(d.word);
+  	nounToNum[d.word] = i;
+	});
+});
 
 // on change of input
 $("#input").keyup(function(){
 	var value = $("#input").val();
+	if(value==""){
+		$("#output").text("super awesome tool");
+	}
 	// if number
-	if(hasNumber(value)){
+	else if(hasNumber(value)){
 		// remove spaces
 		value = value.replace(/\s/g, '');
 		// split into 3, 3, 4
@@ -25,14 +46,18 @@ $("#input").keyup(function(){
 		var val2 = parseInt(value.substring(3,6));
 		var val3 = parseInt(value.substring(6,10));
 		// return corresponding words
-		$("#output").text(checkDef(numToAdj[val1]) + " " + checkDef(numToAdj[val2]) + " " + checkDef(numToNoun[val3]));
+		$("#output").text(checkDef(numToAdj[val1])
+			+ " " + checkDef(numToAdj[val2])
+			+ " " + checkDef(numToNoun[val3]));
 	}
 	// else if string
 	else {
 		// remove spaces
-		var vals = value.split(" ");
+		var vals = value.toLowerCase().split(" ");
 		// return corresponding number
-		$("#output").text(checkDef(adjToNum[vals[0]]) + "-" + checkDef(adjToNum[vals[1]]) + "-" + checkDef(nounToNum[vals[2]]));
+		$("#output").text(pad(checkDef(adjToNum[vals[0]]),3)
+			+ "-" + pad(checkDef(adjToNum[vals[1]]),3)
+			+ "-" + pad(checkDef(nounToNum[checkDef(vals[2])]),4));
 	}
 });
 
@@ -42,10 +67,17 @@ function hasNumber(myString) {
 }
 // returns blank instead of undefined
 function checkDef(value) {
-	if(value==undefined){
+	if(value==undefined || value==null){
 		return "________";
 	} else {
 		return value;
 	}
+}
+// returns 0 padded number
+function pad(n, width, z) {
+	if(!hasNumber(n)) return n;
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
